878753184d7db5c89b6e2746a6f7b51182f3cc94
Joe Colandro <jcolandro@macbookjoe.local>
2020-03-26 17:43:32 -0400

    added outputs

--- infrastructure-as-code/vmw-vm/main.tf
+++ infrastructure-as-code/vmw-vm/main.tf
@@ -1,3 +1,14 @@
+
+
+
+provider "vsphere" {
+  user           = "${var.vsphere_user}"
+  password       = "${var.vsphere_password}"
+  vsphere_server = "${var.vsphere_server}"
+  allow_unverified_ssl = true
+}
+
+
 data "vsphere_datacenter" "dc" {
   name = "PacketDatacenter"
 }
--- infrastructure-as-code/vmw-vm/variables.tf
+++ infrastructure-as-code/vmw-vm/variables.tf
@@ -1,18 +1,15 @@
 variable "vsphere_server" {
     description = "vsphere server for the environment - EXAMPLE: vcenter01.hosted.local"
-    default = "vcenter.test.local"
+    default = "10.100.0.3"
 }
 variable "vsphere_user" {
-    descriping 10ption = "vsphere server for the environment - EXAMPLE: vsphereuser"
+    description = "vsphere server for the environment - EXAMPLE: vsphereuser"
     default = "administrator@vsphere.local"
 }
 variable "vsphere_password" {
     description = "vsphere server password for the environment"
 }
-variable "rpm" {
-    description = "rpm for software install"
-    default = "installer.rpm"
-}
+
 variable "password" {
     description = "Root account password"
 }
