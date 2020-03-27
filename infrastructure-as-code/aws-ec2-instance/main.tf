c00425b3cff7fa1815db4361c04e2d6421e2ce8f
Joe Colandro <jcolandro@macbookjoe.local>
2020-03-26 21:43:53 -0400

    tags

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -10,8 +10,4 @@ resource "aws_instance" "ubuntu" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   availability_zone = "${var.aws_region}a"
-
-  tags {
-    Name = "${var.name}"
-  }
 }
